e344cfff2960f77a6811d4601d80436100a081db
Vansh Mahalle <vanshmahale055@gmail.com>
2026-06-22 03:05:34 +0530

    Initial commit

---
+++ nowthink/src/main/java/com/nowthink/NowthinkApplication.java
@@ -0,0 +1,13 @@
+package com.nowthink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowthinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowthinkApplication.class, args);
+	}
+
+}
---
+++ nowthink/src/test/java/com/nowthink/NowthinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowthink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowthinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
